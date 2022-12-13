b45b212035033f8a7ad408c6c2d1ca8b2ef7bd78
csuastt <906284958@qq.com>
2022-12-13 14:30:47 +0800

    Modify model

--- src/nufno/nufno_2d.py
+++ src/nufno/nufno_2d.py
@@ -3,46 +3,65 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 ################################################################
-#  1d fourier layer
+# fourier layer
 ################################################################
-class SpectralConv1d(nn.Module):
-    def __init__(self, in_channels, out_channels, modes1):
-        super(SpectralConv1d, self).__init__()
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
 
         """
-        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
         """
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes1 = modes1  #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes1 = modes1    # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
 
-        self.scale = (1 / (in_channels*out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat))
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
 
     # Complex multiplication
-    def compl_mul1d(self, input, weights):
-        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
-        return torch.einsum("bix,iox->box", input, weights)
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
 
     def forward(self, x):
         batchsize = x.shape[0]
         #Compute Fourier coeffcients up to factor of e^(- something constant)
-        x_ft = torch.fft.rfft(x)
+        x_ft = torch.fft.rfft2(x)
 
         # Multiply relevant Fourier modes
-        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-1)//2 + 1,  device=x.device, dtype=torch.cfloat)
-        out_ft[:, :, :self.modes1] = self.compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
 
         #Return to physical space
-        x = torch.fft.irfft(out_ft, n=x.size(-1))
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+    
+    def forward_ft(self, x_ft, width1, width2):
+        x_ft1 = self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        x_ft2 = self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(x_ft.shape, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1, :self.modes2] = x_ft1
+        out_ft[:, :, -self.modes1:, :self.modes2] = x_ft2
+
+        #Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(width1, width2))
         return x
 
-class NUFNO(nn.Module):
-    def __init__(self, modes, width):
-        super(NUFNO, self).__init__()
+
+class NUFNO2d(nn.Module):
+    def __init__(self, modes1=16, modes2=16, width1=32, width2=32, 
+        n_channels_global=32, n_channels_local=8, n_subdomains=32, dim_in=1, dim_out=1):
+        super(NUFNO2d, self).__init__()
 
         """
         The overall network. It contains 4 layers of the Fourier layer.
@@ -51,66 +70,170 @@ class NUFNO(nn.Module):
             W defined by self.w; K defined by self.conv .
         3. Project from the channel space to the output space by self.fc1 and self.fc2 .
         
-        input: the solution of the initial condition and location (a(x), x)
-        input shape: (batchsize, x=s, c=2)
-        output: the solution of a later timestep
-        output shape: (batchsize, x=s, c=1)
+        input: the solution of the coefficient function and locations (a(x, y), x, y)
+        input shape: (batchsize, x=s, y=s, c=3)
+        output: the solution 
+        output shape: (batchsize, x=s, y=s, c=1)
         """
 
-        self.modes1 = modes
-        self.width = width
-        self.padding = 2 # pad the domain if input is non-periodic
-        self.fc_embed_u = nn.Linear(42, 256)
-        self.fc_embed_x = nn.Linear(972, 256)
-        self.de_embed = nn.Linear(256, 972)
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width1 = width1
+        self.width2 = width2
+        self.n_channels_global = n_channels_global
+        self.n_channels_local = n_channels_local
+        self.n_subdomains = n_subdomains
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        self.padding = 9 # pad the domain if input is non-periodic
 
-        self.fc0 = nn.Linear(3, self.width) # input channel is 2: (a(x), x)
+        # Lift the modes (complex number)
+        scale = 2 / np.sqrt(dim_in)
+        self.fc0_weights = nn.Parameter(
+            scale * (torch.rand(self.dim_in, self.n_channels_global, dtype=torch.cfloat) - (0.5 + 0.5j)))
+        self.fc0_bias = nn.Parameter(
+            scale * (torch.rand(1, 1, 1, self.n_channels_global, dtype=torch.cfloat) - (0.5 + 0.5j)))
+        self.fc4_weights = nn.Parameter(
+            scale * (torch.rand(self.dim_in, self.n_channels_local, dtype=torch.cfloat) - (0.5 + 0.5j)))
+        self.fc4_bias = nn.Parameter(
+            scale * (torch.rand(1, 1, 1, self.n_channels_local, dtype=torch.cfloat) - (0.5 + 0.5j)))
 
-        self.conv0 = SpectralConv1d(self.width, self.width, self.modes1)
-        self.conv1 = SpectralConv1d(self.width, self.width, self.modes1)
-        self.conv2 = SpectralConv1d(self.width, self.width, self.modes1)
-        self.conv3 = SpectralConv1d(self.width, self.width, self.modes1)
-        self.w0 = nn.Conv1d(self.width, self.width, 1)
-        self.w1 = nn.Conv1d(self.width, self.width, 1)
-        self.w2 = nn.Conv1d(self.width, self.width, 1)
-        self.w3 = nn.Conv1d(self.width, self.width, 1)
+        self.conv_global0 = SpectralConv2d(self.n_channels_global, self.n_channels_global, self.modes1, self.modes2)
+        self.conv_global1 = SpectralConv2d(self.n_channels_global, self.n_channels_global, self.modes1, self.modes2)
+        self.conv_global2 = SpectralConv2d(self.n_channels_global, self.n_channels_global, self.modes1, self.modes2)
+        self.conv_local0 = SpectralConv2d(self.n_channels_local, self.n_channels_local, self.modes1, self.modes2)
+        self.conv_local1 = SpectralConv2d(self.n_channels_local, self.n_channels_local, self.modes1, self.modes2)
+        self.conv_local2 = SpectralConv2d(self.n_channels_local, self.n_channels_local, self.modes1, self.modes2)
 
-        self.fc1 = nn.Linear(self.width, 128)
-        self.fc2 = nn.Linear(128, 1)
+        self.w_global1 = nn.Conv2d(self.n_channels_global, self.n_channels_global, 1)
+        self.w_global2 = nn.Conv2d(self.n_channels_global, self.n_channels_global, 1)
+        self.w_local1 = nn.Conv2d(self.n_channels_local, self.n_channels_local, 1)
+        self.w_local2 = nn.Conv2d(self.n_channels_local, self.n_channels_local, 1)
 
-    def forward(self, u, x):
-        u = self.fc_embed_u(u).unsqueeze(-1)
-        x = self.fc_embed_x(x.permute(0, 2, 1)).permute(0, 2, 1)
+        self.fc1 = nn.Linear(self.n_channels_global, self.n_channels_local)
+        self.fc2 = nn.Linear(self.n_channels_local, 128)
+        self.fc3 = nn.Linear(128, self.dim_out)
 
-        x = torch.cat((u, x), dim=-1)
-        x = self.fc0(x)
-        x = x.permute(0, 2, 1)
-        # x = F.pad(x, [0,self.padding]) # pad the domain if input is non-periodic
+    def complex_lift(self, x_ft, weight, bias):
+        # (batch, x, y, in_channel), (in_channel, out_channel) -> (batch, x, y, out_channel)
+        return torch.einsum("bxyi,io->bxyo", x_ft, weight) + bias
+    
+    
+    def combine_subdomain_modes(self, x_ft_sd, sd_info):
+        m1 = x_ft_sd.shape[-3]
+        m2 = x_ft_sd.shape[-2]
 
-        x1 = self.conv0(x)
-        x2 = self.w0(x)
-        x = x1 + x2
-        x = F.gelu(x)
+        # Frequency number (m1, m2)
+        ky =  torch.cat((torch.arange(start=0, end=self.modes1, step=1), \
+                            torch.arange(start=-(self.modes1), end=0, step=1)), 0).reshape(m1,1).repeat(1,m2)
+        kx =  torch.arange(start=0, end=self.modes2, step=1).reshape(1,m2).repeat(m1,1)
+        
+        # Combination coefficients
+        # exp(-j * 2pi * (xlen/tot_xlen * kx + ylen/tot_ylen * ky)),
+        # where tot_xlen == tot_ylen == for this case
+        kx = kx.reshape(1, 1, m1, m2)
+        ky = ky.reshape(1, 1, m1, m2)
+        xlen = sd_info[:, :, 2:3].unsqueeze(-1).repeat(1, 1, m1, m2)
+        ylen = sd_info[:, :, 3:4].unsqueeze(-1).repeat(1, 1, m1, m2)
+        coef = torch.exp(-1j * 2 * np.pi * (xlen * kx + ylen * ky)).to(torch.cfloat)
+        
+        # (batch, n_subdomains, m1, m2, dim_in), (batch, n_subdomains, m1, m2) -> (batch, m1, m2, dim_in)
+        return torch.einsum("bnxyc,bnxy->bxyc", x_ft_sd, coef)
 
-        x1 = self.conv1(x)
-        x2 = self.w1(x)
-        x = x1 + x2
-        x = F.gelu(x)
 
-        x1 = self.conv2(x)
-        x2 = self.w2(x)
-        x = x1 + x2
-        x = F.gelu(x)
+    def forward(self, u_sd, xy, ind, sep, sd_info):
+        # u_sd: (batch, n_subdomains, m1, m2, dim_in)
+        u = self.combine_subdomain_modes(u_sd, sd_info)
+        u = self.complex_lift(u, self.fc0_weights, self.fc0_bias)    
+                                    # (batch, m1, m2, n_channels_global)
+        u = u.permute(0, 3, 1, 2)   # (batch, n_channels_global, m1, m2)
 
-        x1 = self.conv3(x)
-        x2 = self.w3(x)
-        x = x1 + x2
+        # Global
+        u = self.conv_global0.forward_ft(u, self.width1, self.width2)
+        u = F.gelu(u)
 
-        # x = x[..., :-self.padding] # pad the domain if input is non-periodic
-        x = x.permute(0, 2, 1)
-        x = self.fc1(x)
-        x = F.gelu(x)
-        x = self.fc2(x)
+        u1 = self.conv_global1(u)
+        u2 = self.w_global1(u)
+        u = u1 + u2
+        u = F.gelu(u)
 
-        x = self.de_embed(x.permute(0, 2, 1)).permute(0, 2, 1)
-        return x
+        u1 = self.conv_global2(u)
+        u2 = self.w_global2(u)
+        u = u1 + u2
+        u = F.gelu(u)               # (batch, n_channels_global, w1, w2)
+
+        # Local
+        u = u.permute(0, 2, 3, 1)   # (batch, w1, w2, n_channels_global)
+        u = self.fc1(u)             # (batch, w1, w2, n_channels_local)
+        
+        u = u.permute(0, 3, 1, 2)   # (batch, n_channels_local, m1, m2)
+        u_ft = torch.fft.rfft2(u)
+        u_ft = torch.concat((
+            u_ft[:, :, :self.modes1, :self.modes2],
+            u_ft[:, :, -self.modes1:, :self.modes2]), dim=-2)
+                                    # (batch, n_channels_local, m1, m2)
+        u_ft = torch.repeat_interleave(u_ft, self.n_subdomains, dim=0)
+                                    # (batch * n_subdomains, n_channels_local, m1, m2)
+        u = self.complex_lift(u_sd.reshape(-1, u_sd.shape[2], u_sd.shape[3], self.dim_in),
+            self.fc4_weights, self.fc4_bias).permute(0, 3, 1, 2) + u_ft
+        u = self.conv_local0.forward_ft(u, self.width1, self.width2)
+        u = F.gelu(u)
+
+        u1 = self.conv_local1(u)
+        u2 = self.w_local1(u)
+        u = u1 + u2
+        u = F.gelu(u)
+
+        u1 = self.conv_local2(u)
+        u2 = self.w_local2(u)
+        u = u1 + u2 # (batch * n_subdomains, n_channels_local, w1, w2)
+
+        # Interpolation (from subdomain grids to point cloud)
+        sd_xy = self.get_subdomains_xy(xy, ind, sep, sd_info)
+            # Output shape: (batch * n_subdomains, n_points, 1, 2)
+        u = F.grid_sample(input=u, grid=sd_xy, padding_mode='border', align_corners=False)
+            # Output shape: (batch * n_subdomains, n_channels_local, n_points, 1)
+        u = u.squeeze(-1).permute(0, 2, 1)
+            # Output shape: (batch * n_subdomains, n_points, n_channels_local)
+        u = self.gather_subdomains(u, ind, sep)
+        
+        u = self.fc2(u)
+        u = F.gelu(u)
+        u = self.fc3(u)
+
+        return u
+    
+    def get_subdomains_xy(self, xy, ind, sep, sd_info):
+        # ind (batch, Nx) the input value
+        # sep (batch, N_subdomains+1) the input value
+        # subdomain_info (batch, N_subdomains, 7) the input value
+        # Statics
+        batch_size = xy.shape[0]
+        max_n_points = torch.max(sd_info[:, :, -1]).to(torch.int)
+        # Padding
+        sd_xy = torch.zeros(batch_size * self.n_subdomains, max_n_points, 2)
+        for b in range(batch_size):
+            for j in range(self.n_subdomains):
+                indices = ind[b, sep[b, j]:sep[b, j+1]]
+                loc = xy[b, indices, :]
+                # Normalize to [-1, 1]
+                _min, _len = sd_info[b, j:j+1, :2], sd_info[b, j:j+1, 2:4]
+                loc = (loc - _min) / _len * 2 - 1
+                sd_xy[b * self.n_subdomains + j, :len(indices), :] = loc
+        
+        return sd_xy.unsqueeze(-2)
+    
+    def gather_subdomains(self, u_sd, ind, sep):
+        # u (batch, Nx, width) the input value
+        # ind (batch, Nx) the input value
+        # sep (batch, N_subdomains+1) the input value
+        # Statics
+        batch_size = ind.shape[0]
+        u = torch.zeros(batch_size, ind.shape[1], self.n_channels_local)
+        for b in range(batch_size):
+            for j in range(self.n_subdomains):
+                indices = ind[b, sep[b, j]:sep[b, j+1]]
+                u[b, indices, :] = \
+                    u_sd[b * self.n_subdomains + j, :len(indices), :]
+
+        return u
